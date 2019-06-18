85d7cce53237d1f82694b76d9b0435f07bd53bed
John Doe <30752740@qq.com>
2019-06-18 11:07:56 +0800

    Initial commit

---
+++ gmall0105-logger/src/main/java/com/atguigu/dw/gmall0105logger/Gmall0105LoggerApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.dw.gmall0105logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0105LoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0105LoggerApplication.class, args);
+    }
+
+}
