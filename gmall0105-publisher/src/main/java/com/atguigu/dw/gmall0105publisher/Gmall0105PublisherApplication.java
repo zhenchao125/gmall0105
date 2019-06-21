fea6173ef8ac1b16943fa91249fe75ceb82d696e
John Doe <30752740@qq.com>
2019-06-21 10:00:09 +0800

    更新

---
+++ gmall0105-publisher/src/main/java/com/atguigu/dw/gmall0105publisher/Gmall0105PublisherApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.dw.gmall0105publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0105PublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0105PublisherApplication.class, args);
+    }
+
+}
